0f1160019bde8862a20c079b13124f36c44d3d34
jgutierrez9891 <j.gutierrezl@uniandes.edu.co>
2023-11-02 05:12:29 -0500

    Cambios para incluir dialog para agregar info tecnica

--- src/app/candidato/candidato-infoTecnica/candidatoInfoTecnica/candidatoInfoTecnica.component.ts
+++ src/app/candidato/candidato-infoTecnica/candidatoInfoTecnica/candidatoInfoTecnica.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CandidatoCrearService } from 'app/candidato/candidatoCrear.service';
 import { Router } from '@angular/router';
+import {MatDialog, MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+export interface DialogData {
+  tipo: string;
+  valor: string;
+}
 
 @Component({
   selector: 'app-candidatoInfoTecnica',
@@ -9,9 +19,13 @@ import { Router } from '@angular/router';
 })
 export class CandidatoInfoTecnicaComponent implements OnInit {
 
+  tipo: string;
+  valor: string;
+
   constructor(
     private candidatoService: CandidatoCrearService,
-    private _router: Router
+    private _router: Router,
+    //public dialog: MatDialog,
   ) { }
 
   lista:any = {};
@@ -26,9 +40,43 @@ export class CandidatoInfoTecnicaComponent implements OnInit {
     })
   }
 
-
   backToInfo(){
     this._router.navigate(["editarCandidatos"])
   }
 
+  openDialog(): void {
+    /*const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: {tipo: this.tipo, valor: this.valor},
+      height: '400px',
+      width: '600px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.tipo = result.tipo,
+      this.valor = result.valor
+      console.log("tipo: ", this.tipo)
+      console.log("valor: ", this.valor)
+      //this.guardarInfoTecnica()
+    });*/
+  }
+
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+  standalone: true,
+  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule],
+})
+
+export class DialogOverviewExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
